37002a2d363b887f254ae1c4581ce89eafd89320
jordyaraujo <jordyaraujo@outlook.com>
2022-04-26 13:58:24 -0300

    Lesson 01 - Liftoff
    - Initialize App;
    - Create Splash Screen;
    - Create ButtonIcon Component;
    - Create SignIn Screen.

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Text, View, Image, StatusBar } from "react-native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from "../../assets/illustration.png";
+import { styles } from "./styles";
+
+export function SignIn() {
+  const [text, setText] = useState("");
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize{`\n`}
+          suas jogatinas{`\n`}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games{`\n`}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+      </View>
+    </View>
+  );
+}
